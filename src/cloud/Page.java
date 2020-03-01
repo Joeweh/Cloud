9c6e9d80e394eacf47f7a54484c7ce0875f3501f
Obese-Clown <43752986+Obese-Clown@users.noreply.github.com>
2020-02-29 21:08:07 -0500

    Update Page.java

--- src/cloud/Page.java
+++ src/cloud/Page.java
@@ -1,1 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Page {
+	protected Map<String, Page> pages = new HashMap<>();
+	
+	public Scene setupMainScene(Stage stage) 
+	{
+		return null;
+	}
+	
+	public void addPage(Page page, String name) 
+	{
+		this.pages.put(name, page);
+	}
+}
