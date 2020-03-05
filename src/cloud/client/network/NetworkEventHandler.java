4f746e7ae98edbaee81206694a8de35e324b386f
Obese-Clown <43752986+Obese-Clown@users.noreply.github.com>
2020-03-05 00:54:58 -0500

    Update NetworkEventHandler.java

--- src/cloud/client/network/NetworkEventHandler.java
+++ src/cloud/client/network/NetworkEventHandler.java
@@ -1,9 +1,1 @@
-public class NetworkEvent {
-	final String event;
-	final String dataJSON;
-	
-	NetworkEvent(String event, String dataJSON) {
-		this.event = event;
-		this.dataJSON = dataJSON;
-	}
-}
+
