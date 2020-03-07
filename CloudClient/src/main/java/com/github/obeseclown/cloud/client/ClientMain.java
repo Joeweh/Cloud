ec766aa13f608230bf709ac4bba5b319b5bc9992
Daniel Meylakh <danielmey626@gmail.com>
2020-03-07 01:50:39 -0500

    finally got gradle to work
    all of this code is literally just to make two JARs

---
+++ CloudClient/src/main/java/com/github/obeseclown/cloud/client/ClientMain.java
@@ -0,0 +1,7 @@
+package com.github.obeseclown.cloud.client;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        System.out.println("Client!");
+    }
+}
---
+++ CloudServer/src/main/java/com/github/obeseclown/cloud/server/ServerMain.java
@@ -0,0 +1,7 @@
+package com.github.obeseclown.cloud.server;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("Server!");
+    }
+}
